267f4da475c9aa4582a55f573a35edfd2c0d23b8
Luis Pagliaro <luis.pagliaro@varsitytutors.com>
2023-10-19 11:29:45 -0300

    llp support

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,4 @@
-const platform = 'meet';
+const platform = window.history.state.url.includes("meet") ? "meet" : "llp";
 
 const defaultValue = {
   neutral: 0,
@@ -13,14 +13,18 @@ const defaultValue = {
 let currentEmotionElement = null;
 
 const getVideoInGoogleMeet = () => {
-  const videos = document.querySelectorAll('video');
-  
-  const video = [...videos].find((video) => video.classList.length === 1);
+  const videos = [...document.querySelectorAll("video")];
+
+  if (videos.length === 1) {
+    return;
+  }
+
+  const video = videos.find((video) => video.classList.length === 1);
 
   return video;
 };
 
-const getVideoInLLP = () => document.querySelector('.OT_subscriber video');
+const getVideoInLLP = () => document.querySelector(".OT_subscriber video");
 
 const getVideoPlatforms = {
   meet: getVideoInGoogleMeet,
@@ -30,45 +34,47 @@ const getVideoPlatforms = {
 const showEmotion = (video, expressions = defaultValue) => {
   const { parentNode: videoParent } = video;
 
-  const emotionText = document.createElement('p');
+  const emotionText = document.createElement("p");
 
   Object.assign(emotionText.style, {
-    fontSize: '2rem',
-    fontWeight: 'bold',
-    position: 'absolute',
-    top: '10px',
-    left: '0',
-    width: '100%',
-    textAlign: 'center',
+    fontSize: "2rem",
+    fontWeight: "bold",
+    position: "absolute",
+    top: "20px",
+    left: "0",
+    width: "100%",
+    textAlign: "center",
   });
 
-  const emotion = Object.entries(expressions).reduce((acc, [key, value]) => {
-    if (value > acc.value) {
-      return { key, value };
-    }
+  const emotion = Object.entries(expressions).reduce(
+    (acc, [key, value]) => {
+      if (value > acc.value) {
+        return { key, value };
+      }
 
-    return acc;
-  }, { key: '', value: 0 });
-
+      return acc;
+    },
+    { key: "", value: 0 }
+  );
 
   emotionText.innerText = emotion.key;
 
   if (currentEmotionElement) {
+    if (currentEmotionElement.innerText === emotionText.innerText) {
+      return;
+    }
+
     currentEmotionElement.remove();
   }
 
-
   currentEmotionElement = emotionText;
-
 
   if (emotion.value > 0) {
     videoParent.appendChild(emotionText);
   }
-
 };
 
 const startScript = (video) => {
-
   setInterval(async () => {
     const detections = await faceapi
       .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
@@ -77,13 +83,13 @@ const startScript = (video) => {
     const { expressions } = detections?.[0] ?? {};
 
     showEmotion(video, expressions);
-  }, 50)
+  }, 50);
 };
 
 const getVideo = () => {
   const interval = setInterval(() => {
     const video = getVideoPlatforms[platform]();
-  
+
     if (video) {
       clearInterval(interval);
       startScript(video);
@@ -91,15 +97,19 @@ const getVideo = () => {
   }, 1000);
 };
 
-
 const main = async () => {
   await Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri(chrome.runtime.getURL('/models')),
-    faceapi.nets.faceLandmark68Net.loadFromUri(chrome.runtime.getURL('/models')),
-    faceapi.nets.faceRecognitionNet.loadFromUri(chrome.runtime.getURL('/models')),
-    faceapi.nets.faceExpressionNet.loadFromUri(chrome.runtime.getURL('/models'))
+    faceapi.nets.tinyFaceDetector.loadFromUri(chrome.runtime.getURL("/models")),
+    faceapi.nets.faceLandmark68Net.loadFromUri(
+      chrome.runtime.getURL("/models")
+    ),
+    faceapi.nets.faceRecognitionNet.loadFromUri(
+      chrome.runtime.getURL("/models")
+    ),
+    faceapi.nets.faceExpressionNet.loadFromUri(
+      chrome.runtime.getURL("/models")
+    ),
   ]);
-
 
   getVideo();
 };
